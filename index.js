d84dcd56e1657bc402432b3ba4701e2a05e486eb
afshar360 <ali@yumbusinc.com>
2022-11-23 14:05:08 -0800

    fixes script tag

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const saveEl = document.getElementById("save-el")
 const countEl = document.getElementById("count-el")
+const incBtn = document.getElementById('increment-btn')
+const decBtn = document.getElementById('decrement-btn')
 let count = 0
+
+incBtn.addEventListener('click', increment)
+decBtn.addEventListener('click', decrement)
 
 function increment() {
     count += 1
