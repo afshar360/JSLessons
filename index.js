67329cd45810aeec56e4ab6e713bf9e8c03df056
afshar360 <ali@yumbusinc.com>
2022-11-28 15:44:04 -0800

    added h1 id to h1 element

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const saveBtn = document.getElementById('save-btn')
 const countEl = document.getElementById("count-el")
 const incBtn = document.getElementById('increment-btn')
 const decBtn = document.getElementById('decrement-btn')
+const h1=document.getElementById('h1-id')
 let count = 0
 
 incBtn.addEventListener('click', increment)
@@ -13,11 +14,13 @@ saveBtn.addEventListener('click', save)
 function increment() {
     count += 1
     countEl.textContent = count
+    h1.textContent = "I just added this text to this element"
 }
 
 function decrement() {
     count -= 1
     countEl.textContent = count
+    h1.textContent = "I just deducted this text from this element"
 }
 
 function save() {
@@ -26,5 +29,6 @@ function save() {
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
+    h1.textContent = "I just saved this text to this element"
 }
 
